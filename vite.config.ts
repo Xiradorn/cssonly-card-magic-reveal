8fc71a2573176119a3a96441ecd947914782f7a0
Xiradorn <xiradorn@gmail.com>
2024-08-04 15:14:19 +0200

    feat: First commit :circus_tent:

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		server: {
+			port: env.VITE_SERVER_PORT,
+		},
+	};
+});
